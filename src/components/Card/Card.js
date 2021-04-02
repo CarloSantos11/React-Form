b50f13682faf540bd8577536194f4d46c7a3c326
Jared Hightower <Jared.jhightower@gmail.com>
2021-04-02 14:18:23 -0500

    added online offline status but still needs work.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,16 +1,17 @@
 import { FaGithub, FaDiscord, FaInstagram, FaSlackHash } from "react-icons/fa";
-import styled, { css } from 'styled-components'
+import styled, { css } from "styled-components";
+//import { useState } from "react";
 
 let socilaIcons = {
-  github: <FaGithub size={30}/>,
-  discord: <FaDiscord size={30}/>,
-  instagram: <FaInstagram size={30}/>,
-  slack: <FaSlackHash size={30}/>,
-}
+  github: <FaGithub size={30} />,
+  discord: <FaDiscord size={30} />,
+  instagram: <FaInstagram size={30} />,
+  slack: <FaSlackHash size={30} />,
+};
 
 const LanguageName = styled.p`
   text-transform: capitalize;
-  `
+`;
 
 const CardView = styled.section`
   width: 500px;
@@ -18,54 +19,88 @@ const CardView = styled.section`
   background-color: #fff;
   padding: 2rem 0rem 3rem 5rem;
   margin-bottom: 10rem;
-  -webkit-box-shadow: 8px 7px 11px 6px rgba(163,163,163,0.7);
-  box-shadow: 8px 7px 11px 6px rgba(163,163,163,0.7);
-`
+  -webkit-box-shadow: 8px 7px 11px 6px rgba(163, 163, 163, 0.7);
+  box-shadow: 8px 7px 11px 6px rgba(163, 163, 163, 0.7);
+`;
 const Container = styled.section`
-    display: flex;
-`
+  display: flex;
+`;
 
 const Column = styled.section`
   display: inline-block;
   padding: 2rem 5rem 0 0;
-`
+`;
+
+// let noneActiveUserStyle = styled.section`
+//   color: #c0b5b5;
+// `;
+
 // const ColumnTwo = styled.section`
 //   display: inline-block;
 // `
 
-function Card(props) {
-  const socialMedia = props.userInfo.socialMedia;
+/**
+ * Refactored userInfo
+ * Added conditional rendering to set active user to true
+ * Card component now displays online and offline status
+ * ? Need to figure out how to pass state for login to card view form.
+ * ? Login converts active from false to true but when user is rendered to "/index"
+ * ? active is converted back to a false value.
+ */
+
+function Card({ userInfo }) {
+  const socialMedia = userInfo.socialMedia;
+  const activeUser = !userInfo.active;
+  console.log(activeUser);
+
   return (
     <CardView>
-      <h1>{props.userInfo.username}</h1>
+      <h1>{userInfo.username}</h1>
+      {activeUser ? (
+        <h5 style={{ color: "green" }}>online</h5>
+      ) : (
+        !activeUser && <h5 style={{ color: "gray" }}>offline</h5>
+      )}
+
+      {/* {activeUser && <h5 style={{ color: "green" }}>online</h5>}
+      {!activeUser && <h5 style={{ display: "none" }}>online</h5>} */}
       <Container>
         <Column>
           <h2>Role</h2>
-          {props.userInfo.role.map((currentRole) => (
-            currentRole.value && <h4>{currentRole.name}</h4>
-          ))}
-          
+          {userInfo.role.map(
+            (currentRole) => currentRole.value && <h4>{currentRole.name}</h4>
+          )}
         </Column>
         <Column>
           <h2>Languages</h2>
-          {props.userInfo.socialMedia.map((social) => console.log(social)) }
-          {props.userInfo.languages.map((language) => (
-            language.value && <LanguageName>{language.name}</LanguageName>
-          ))}
+          {userInfo.socialMedia.map((social) => console.log(social))}
+          {userInfo.languages.map(
+            (language) =>
+              language.value && <LanguageName>{language.name}</LanguageName>
+          )}
 
           <h3>Social Media</h3>
           <div id="post_social_links">
-            {socialMedia.map((social) => (
-              social.value && <a style={{ padding: '0.2rem' }} href={social.value} target="_blank">{socilaIcons[social.name]}</a>
-            ))}
+            {socialMedia.map(
+              (social) =>
+                social.value && (
+                  <a
+                    style={{ padding: "0.2rem" }}
+                    href={social.value}
+                    target="_blank"
+                  >
+                    {socilaIcons[social.name]}
+                  </a>
+                )
+            )}
           </div>
         </Column>
       </Container>
     </CardView>
-  )
+  );
 }
 
-export default Card
+export default Card;
 
 // React router
 // - Login
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,79 +1,89 @@
-import React, { useState } from 'react';
-import users from '../../mock-db/users.json'
-import PropTypes from 'prop-types';
-
+import React, { useState } from "react";
+import users from "../../mock-db/users.json";
 
 const outerSectionStyle = {
-  textAlign: 'center',
-  width: '50%',
-  padding: '20px 16px',
-  margin: '20px auto'
-}
+  textAlign: "center",
+  width: "50%",
+  padding: "20px 16px",
+  margin: "20px auto",
+};
 
 const Login = (props) => {
   const [usernameField, setUsernameField] = useState("");
   const [passwordField, setPasswordField] = useState("");
+  const [activeUser, setActiveUser] = useState(false);
   const [user, setUser] = useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
     users.forEach((currentUser) => {
-      if (currentUser?.username === usernameField &&
-        currentUser?.password === passwordField) {
+      if (
+        currentUser?.username === usernameField &&
+        currentUser?.password === passwordField
+      ) {
         setUser(currentUser);
       }
+      //! works but creates duplicate logs? WHY
+      if (currentUser?.active === false) {
+        setActiveUser((currentUser = true));
+        console.log(currentUser);
+      }
     });
-    props.onChange(user);
-
+    props.onChange(user, activeUser);
   }
 
   function handleNameChange(e) {
-    setUsernameField(e.target.value)
+    setUsernameField(e.target.value);
   }
 
   function handlePasswordChange(e) {
-    setPasswordField(e.target.value)
+    setPasswordField(e.target.value);
   }
 
   return (
     <div style={outerSectionStyle}>
-      <h1>Login</h1><br />
-      <form
-        name="form"
-        id="login"
-        onSubmit={handleSubmit}
-      >
+      <h1>Login</h1>
+      <br />
+      <form name="form" id="login" onSubmit={handleSubmit}>
         <div id="username">
-          <label htmlFor="username">User Name</label><br />
+          <label htmlFor="username">User Name</label>
+          <br />
           <input
             onChange={handleNameChange}
             type="text"
             name="username"
             value={usernameField}
             placeholder="Username"
-          /><br />
+          />
+          <br />
         </div>
 
         <div id="password">
-          <label htmlFor="password">Password</label><br />
+          <label htmlFor="password">Password</label>
+          <br />
           <input
             onChange={handlePasswordChange}
             type="text"
             name="password"
             value={passwordField}
             placeholder="Password"
-          /><br />
+          />
+          <br />
         </div>
         <br />
-        <button className="btn btn-danger" type="submit" style={{marginRight: '10px'}}>Submit</button>
-        <button className="btn btn-danger" type="submit"><a href="/signup">Signup</a></button>
+        <button
+          className="btn btn-danger"
+          type="submit"
+          style={{ marginRight: "10px" }}
+        >
+          Submit
+        </button>
+        <button className="btn btn-danger" type="submit">
+          <a href="/signup">Signup</a>
+        </button>
       </form>
     </div>
   );
 };
-
-
-
-
 
 export default Login;
