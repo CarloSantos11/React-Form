f61effa96762278385436f6ce57b1f862d8cd164
Carlo Santos <carlo@snapit.solutions>
2021-04-15 00:49:55 -0500

    working navigation

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,12 @@ import "../index.css";
 import Form from "./Form";
 import IndexView from "./IndexView";
 import Login from "./Login";
+import Nav from "./Nav";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
-  Link,
 } from "react-router-dom";
 
 function App() {
@@ -16,37 +16,30 @@ function App() {
 
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/signup">Signup</Link>
-            </li>
-            <li>
-              <Link to="/">logout</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-
       <Switch>
-        <Route path="/" exact>
-          {user ? (
-            <Redirect to="/index" />
-          ) : (
-            <Login setUser={setUser} /> 
-          )}
-          {/*protected route*/}
-        </Route>
-        <Route path="/signup">
-          <Form />
-        </Route>
-        <Route path="/index">
-          <IndexView />
-        </Route>
+
+        <>
+
+          {user && <Nav />}                       
+        
+          <Route path="/" exact>
+            {user ? (                             // If User is present
+              <Redirect to="/index" />            // Display index
+            ) : (                                 // Otherwise
+              <Login setUser={setUser} />         // Display Login
+            )}
+          </Route>
+
+          <Route path="/signup">
+            <Form />
+          </Route>
+
+          <Route path="/index">
+            <IndexView />
+          </Route>
+
+        </>
+
       </Switch>
     </Router>
   );
